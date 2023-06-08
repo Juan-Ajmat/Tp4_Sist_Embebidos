5547f4afdf4e657945841c64ed2f6eb23ceba561
Juan_Ajmat <juan.ajmat@gmail.com>
2023-06-07 21:35:45 -0300

    ComienzoTp6

---
+++ inc/pantalla.h
@@ -0,0 +1,115 @@
+/* Copyright 2022, Laboratorio de Microprocesadores
+ * Facultad de Ciencias Exactas y Tecnología
+ * Universidad Nacional de Tucuman
+ * http://www.microprocesadores.unt.edu.ar/
+ * Copyright 2022, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \brief Simple sample of use LPC HAL gpio functions
+ **
+ ** \addtogroup samples Sample projects
+ ** \brief Sample projects to use as a starting point
+ ** @{ */
+
+/* === Headers files inclusions =============================================================== */
+#include <stdint.h>
+/* === Cabecera C++ =========================================================================== */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Macros definitions ====================================================================== */
+
+// Definicion de bits asociados a cada segmento para cpnstruir los digitos
+
+#define SEGMENT_A (1 << 0) 
+#define SEGMENT_B (1 << 1)
+#define SEGMENT_C (1 << 2)
+#define SEGMENT_D (1 << 3)
+#define SEGMENT_E (1 << 4)
+#define SEGMENT_F (1 << 5)
+#define SEGMENT_G (1 << 6)
+#define SEGMENT_P (1 << 7)
+
+/* === Public data type declarations ========================================================== */
+typedef struct display_s * display_t;
+
+
+typedef void(*display_screen_off_t)(void);
+
+typedef void(*display_segments_on_t)(uint8_t segments);
+
+typedef void(*display_digit_on_t)(uint8_t digit);
+
+typedef struct displat_driver_s{
+    display_screen_off_t ApagoPantalla;
+    display_segments_on_t PrendoSegmentos;
+    display_digit_on_t PrendoDigito;
+}const * const displat_driver_t;
+
+/* === Private data type declarations ========================================================== */
+
+struct display_s{
+    uint8_t digitos;
+    uint8_t digito_activo;
+    uint8_t memory[DISPLAY_MAX_DIGITS];
+    struct display_driver_s driver[1];
+}
+
+/* === Public variable declarations =========================================================== */
+
+display_t DisplayCreate(uint8_t digits, displat_driver_t driver);
+
+
+void DisplayWriteBCD(display_t display, uint8_t * number, uint8_t size);
+
+
+void DisplayRefresh(display_t display);
+
+
+/* === Private variable declarations =========================================================== */
+
+/* === Public function declarations =========================================================== */
+
+/* === Private function declarations =========================================================== */
+
+/* === Public variable definitions ============================================================= */
+
+/* === Private variable definitions ============================================================ */
+
+/* === Private function implementation ========================================================= */
+
+/* === Public function implementation ========================================================= */
+
+/* === End of documentation ==================================================================== */
+#ifdef __cplusplus
+}
+#endif
+/** @} End of module definition for doxygen */
